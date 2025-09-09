088e383baa5a4b1e66193f20c8fcd8bdde838f98
Chris M <M-Chris@users.noreply.github.com>
2025-09-09 17:19:35 -0600

    test: verify pre-commit hook formatting

---
+++ test-formatting.ts
@@ -0,0 +1,6 @@
+// Test file with intentionally bad formatting
+const badlyFormatted = { a: 1, b: 2, c: 3 };
+function test(x: number, y: string) {
+  return x + ' ' + y;
+}
+const arr = [1, 2, 3, 4, 5];
